346f18b53f2951c5f67f1c0305b899e5c09e8269
Tim Dobson <hello@tdobson.net>
2024-10-14 16:41:06 +0100

    fix: update Lambda function CodeUri and Handler to use dist folder

--- device-magic-proxy/app.ts
+++ device-magic-proxy/app.ts
@@ -1,8 +1,9 @@
 import fs from "fs";
+import path from 'path';
 import axios from "axios";
 import { Config, Event, Context, Response } from "./types";
 
-const config: Config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+const config: Config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf8"));
 
 export const lambdaHandler = async (event: Event, context: Context): Promise<Response> => {
     try {
