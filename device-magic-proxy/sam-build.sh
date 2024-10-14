e1c2a2fe9e42dd62a65eca356278427eaa6aab65
Tim Dobson <hello@tdobson.net>
2024-10-14 16:52:27 +0100

    fix: Add debugging information to build scripts

--- device-magic-proxy/sam-build.sh
+++ device-magic-proxy/sam-build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -e
+set -x
+
+echo "Starting sam-build.sh"
 npm ci
 npm run build
 cp -r node_modules dist/
+echo "Contents of dist directory:"
+ls -R dist/
+echo "End of sam-build.sh"
