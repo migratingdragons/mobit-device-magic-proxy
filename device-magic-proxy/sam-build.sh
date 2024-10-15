240c4103c02a37a43da131d0f20de4bb4369f628
Tim Dobson <hello@tdobson.net>
2024-10-15 15:34:04 +0100

    fix: update Lambda function configuration to correctly point to entry point

--- device-magic-proxy/sam-build.sh
+++ device-magic-proxy/sam-build.sh
@@ -5,7 +5,7 @@ set -x
 echo "Starting sam-build.sh"
 npm ci
 npm run build
-cp -r node_modules dist/
-echo "Contents of dist directory:"
-ls -R dist/
+cp -r node_modules .
+echo "Contents of current directory:"
+ls -R
 echo "End of sam-build.sh"
